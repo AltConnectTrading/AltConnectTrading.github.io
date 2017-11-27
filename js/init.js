ec8ecd3c9807f89a5aef924bd7f4d286c4410d89
AltConnectTrading <34037102+AltConnectTrading@users.noreply.github.com>
2017-11-27 18:24:08 +0100

    Initialize

---
+++ js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.card-tabs .tabs').tabs({ 'swipeable': true });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
